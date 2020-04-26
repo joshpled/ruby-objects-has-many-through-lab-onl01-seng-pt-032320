48f427a538896215a7a7db2b5ddad17cf436ec27
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-26 01:02:49 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,20 +1,22 @@
 class Patient
-attr_accessor :name
-  @@all = []
+    attr_accessor :name
+      @@all = []
 
-def initialize(name)
-  @name = name
-  @appointments = []
-  @@all << self
-end
+    def initialize(name)
+      @name = name
+      @appointments = []
+      @@all << self
+    end
 
-def self.all
-  @@all
-end
+    def self.all
+      @@all
+    end
 
-def new_appointment(date, patient=self, doctor)
-  Appointment.new(date, self, doctor)
-end
+    def new_appointment(date, patient=self, doctor)
+      Appointment.new(date, self, doctor)
+    end
 
-
+    def appointments
+        Appointment.all.select {|apt| apt.patient == self}
+    end
 end
